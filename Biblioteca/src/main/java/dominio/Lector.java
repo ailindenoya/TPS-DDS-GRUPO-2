dfb4a0d113d7a9f5899b5d0487b28bd6c5b3aea0
aylencandela <aylencandela@hotmail.com>
2023-04-25 18:54:58 -0300

    intento desarrollar un poco mas biblioteca

--- Biblioteca/src/main/java/dominio/Lector.java
+++ Biblioteca/src/main/java/dominio/Lector.java
@@ -1,25 +1,37 @@
 package dominio;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
 import java.util.List;
 
+@Getter @Setter
 public class Lector {
     private String nombre;
     private String apellido;
-    private Integer cantidadLibros;
     private List<Prestamo> prestamos;
-    private Boolean habilitado;
+    //private Boolean habilitado; //flag para ver que no tenga dias multado
     private List<Multa> multas;
 
-    public Boolean puedePedirNuevoPrestamo(){
-        //TODO
-        return Boolean.TRUE;
+    public Integer calcularCantidadLibrosPrestados(){
+        return prestamos.size();
     }
 
-    public void agregarDiasMultado(Integer dias){
-        //TODO
+    public Boolean estaHabilitado(){
+        //se fija que ningun prestamo este vencido
+        return !prestamos.stream().anyMatch(p -> p.estaVencido());
+    }
+    public Boolean puedePedirNuevoPrestamo(){
+        return this.calcularCantidadLibrosPrestados() < 3 && this.estaHabilitado();
     }
 
     public void tomarPrestadoNuevoLibro(Libro libro){
-        //TODO
+        if (!puedePedirNuevoPrestamo()) {
+            //TODO tirar una excepcion?
+        } else {
+            Prestamo prestamo = new Prestamo(libro, this, LocalDate.now(), 30);
+            prestamos.add(prestamo);
+        }
     }
 }
--- Biblioteca/src/main/java/dominio/Prestamo.java
+++ Biblioteca/src/main/java/dominio/Prestamo.java
@@ -1,15 +1,45 @@
 package dominio;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Getter @Setter
 public class Prestamo {
     private Libro libro;
     private Lector lector;
-    private LocalDate fechaDeRetiro;
-    private LocalDate fechaDeVencimiento;
+    private LocalDate fechaDeRetiro; //fecha que el lector pide el prestamo
+    private LocalDate fechaDeVencimiento; //fecha calculada a partir de los dias que le dan de prestamo
+    private Integer diasDePrestamo; //en el texto dice 30 dias, pero me parecio mejor tenerlo como un atributo por si llega a cambiar
+
+    //para cuando el lector devuelva el libro
+    private LocalDate fechaDeDevolucion;
+
+    public Prestamo(Libro libro, Lector lector, LocalDate fechaDeRetiro, Integer diasDePrestamo) {
+        this.libro = libro;
+        this.lector = lector;
+        this.fechaDeRetiro = fechaDeRetiro;
+        this.fechaDeVencimiento = fechaDeRetiro.plusDays(diasDePrestamo); //se setea al crear un nuevo prestamo
+        this.diasDePrestamo = diasDePrestamo;
+    }
 
     public Integer calcularDiasDeMulta(){
-        //TODO
+        //TODO (fechaDevolucion - fechaVencimiento) * 2
+
         return null;
+    }
+
+    public void devolverLibro() {
+        this.setFechaDeDevolucion(LocalDate.now());
+        if (calcularDiasDeMulta() > 0) {
+            //TODO crea una multa y setea al lector como inhabilitado
+            new Multa();
+        }
+    }
+
+    public Boolean estaVencido() {
+        //TODO me falta chequear que fecha de devolucion este seteada
+        return this.getFechaDeDevolucion().isBefore(LocalDate.now());
     }
 }
